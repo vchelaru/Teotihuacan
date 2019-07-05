479c672989d82ed629f2e2ee886801ce1e628e44
Rick Blaylock <blay4480@gmail.com>
2019-07-04 21:02:16 -0600

    Adding twin stick logic to aiming
    The player now aims in the direction of the last right stick input  or in the world position of the mouse/cursor

--- Teotihuacan/Entities/Player.cs
+++ Teotihuacan/Entities/Player.cs
@@ -8,11 +8,17 @@ using FlatRedBall.AI.Pathfinding;
 using FlatRedBall.Graphics.Animation;
 using FlatRedBall.Graphics.Particle;
 using FlatRedBall.Math.Geometry;
+using Microsoft.Xna.Framework;
+using FlatRedBall.Gui;
 
 namespace Teotihuacan.Entities
 {
 	public partial class Player
 	{
+        TopDownDirection directionAiming;
+        I2DInput rightStick;
+        Vector3 aimingVector;
+
         /// <summary>
         /// Initialization logic which is execute only one time for this Entity (unless the Entity is pooled).
         /// This method is called when the Entity is added to managers. Entities which are instantiated but not
@@ -22,21 +28,55 @@ namespace Teotihuacan.Entities
 		{
             this.PossibleDirections = PossibleDirections.EightWay;
 
+            InitializeTwinStickInput();
 		}
 
-		private void CustomActivity()
+        private void InitializeTwinStickInput()
+        {
+            var inputAsGamepad = InputDevice as Xbox360GamePad;
+            if (inputAsGamepad != null)
+            {
+                rightStick = inputAsGamepad.RightStick;
+            }
+        }
+
+        private void CustomActivity()
 		{
+            DoAimingActivity();
             DoShootingActivity();
 
 		}
 
+        private void DoAimingActivity()
+        {
+            Vector3 newAimingVector = aimingVector;
+            if (rightStick != null)
+            {
+                if (rightStick.Magnitude > 0)
+                {
+                    newAimingVector = new Vector3(rightStick.X, rightStick.Y, 0);
+                }
+            }
+            else
+            {
+
+                Vector3 cursorPosition = new Vector3(GuiManager.Cursor.WorldXAt(Z), GuiManager.Cursor.WorldYAt(Z), 0);
+
+                newAimingVector = cursorPosition - new Vector3(X, Y, 0);
+            }
+
+            //Normalize at the end in case the right stick input is not at max magnitude
+            newAimingVector.Normalize();
+            aimingVector = newAimingVector;
+        }
+
         private void DoShootingActivity()
         {
-            var didShoot = InputDevice.DefaultPrimaryActionInput.WasJustPressed;
+            var didShoot = InputDevice.DefaultPrimaryActionInput.WasJustPressed || InputManager.Mouse.ButtonPushed(Mouse.MouseButtons.LeftButton);
 
             if(didShoot)
             {
-                var direction = this.DirectionFacing.ToVector();
+                var direction = aimingVector;
 
                 var bullet = Factories.BulletFactory.CreateNew(this.X, this.Y);
                 bullet.Velocity = bullet.BulletSpeed * direction;
