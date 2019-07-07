628d451d5a9f5f039424e230a43f91ace735e29e
Victor Chelaru <VicChelaru@gmail.com>
2019-07-06 22:24:45 -0600

    Updated code project to have the hud.

---
+++ Teotihuacan/GumRuntimes/BaseHUDRuntime.cs
@@ -0,0 +1,13 @@
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    
+    namespace Teotihuacan.GumRuntimes
+    {
+        public partial class BaseHUDRuntime
+        {
+            partial void CustomInitialize () 
+            {
+            }
+        }
+    }
