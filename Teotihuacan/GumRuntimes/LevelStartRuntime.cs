42395fb6a9702343c8cbd34640045ae7c8671fe6
Riva <microriva@gmail.com>
2019-07-08 20:29:54 +0200

    Added to Gum project and GameScreenGum: Player Join HUD component (placeholder) Game over component Level start countdown component

---
+++ Teotihuacan/GumRuntimes/LevelStartRuntime.cs
@@ -0,0 +1,13 @@
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    
+    namespace Teotihuacan.GumRuntimes
+    {
+        public partial class LevelStartRuntime
+        {
+            partial void CustomInitialize () 
+            {
+            }
+        }
+    }
