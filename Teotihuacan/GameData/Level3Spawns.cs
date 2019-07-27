a5ac9a25bacaeae11a177adc313d6e705a1fcfdc
Victor Chelaru <VicChelaru@gmail.com>
2019-07-27 14:36:27 -0600

    Finsihed spawns for Level3

--- Teotihuacan/GameData/Level3Spawns.cs
+++ Teotihuacan/GameData/Level3Spawns.cs
@@ -15,6 +15,7 @@ namespace Teotihuacan.GameData
         {
             var wave = AddWave();
 
+            // introduce the suicider fast
             wave.AddMiniWave
             (
                 SuiciderFast, SuiciderFast
@@ -37,6 +38,7 @@ namespace Teotihuacan.GameData
 
             /////////////////////////////////////
 
+            // Single miniwave in this, dangerous if you're not prepared.
             wave = AddWave();
 
             wave.AddMiniWave
@@ -49,7 +51,7 @@ namespace Teotihuacan.GameData
 
             /////////////////////////////////////
 
-
+            // filler wave
             wave = AddWave();
 
             wave.AddMiniWave
@@ -74,6 +76,44 @@ namespace Teotihuacan.GameData
             .CanSpawnAtMultipleSpots = true;
 
             /////////////////////////////////////
+
+            wave = AddWave();
+
+            wave.AddMiniWave
+            (
+                SuiciderFast, SuiciderFast, SuiciderFast,
+
+                Shooter, Shooter, Shooter
+            );
+
+            // guys from everywhere
+            wave.AddMiniWave
+            (
+                Shooter, Shooter, Shooter,
+                Shooter, Shooter, Shooter
+            )
+            .CanSpawnAtMultipleSpots = true;
+
+            wave.AddMiniWave
+            (
+                Shooter, Shooter, Shooter, Shooter, 
+                Suicider, Suicider, SuiciderFast, SuiciderFast
+            )
+            .CanSpawnAtMultipleSpots = true;
+
+
+            ///////////////////////////////////////
+
+            // 2 bosses, but maybe you can use the explosions:
+            wave = AddWave();
+
+            wave.AddMiniWave
+            (
+                Boss, Suicider, Suicider,
+                Boss, Suicider, Suicider
+            )
+            .CanSpawnAtMultipleSpots = true;
+
         }
     }
 }
