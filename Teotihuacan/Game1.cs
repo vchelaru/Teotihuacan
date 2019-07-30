8b9c6d3e093c8ed7a37d6abb5dfa474c29a7a100
Victor Chelaru <VicChelaru@gmail.com>
2019-07-29 21:05:38 -0600

    Expanded level 3 width.

--- Teotihuacan/Game1.cs
+++ Teotihuacan/Game1.cs
@@ -59,7 +59,7 @@ namespace Teotihuacan
 
 			CameraSetup.SetupCamera(SpriteManager.Camera, graphics);
 			GlobalContent.Initialize();
-			FlatRedBall.Screens.ScreenManager.Start(typeof(Teotihuacan.Screens.Level4));
+			FlatRedBall.Screens.ScreenManager.Start(typeof(Teotihuacan.Screens.Level3));
 
             base.Initialize();
         }
