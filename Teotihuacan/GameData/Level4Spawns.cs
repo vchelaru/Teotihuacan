02ad0eb204cb3bee1951a4db5ea91bbc7c6dbbb3
Victor Chelaru <VicChelaru@gmail.com>
2019-07-29 20:47:45 -0600

    Added level 4 spawns. Added new spread shooter enemy.

--- Teotihuacan/GameData/Level4Spawns.cs
+++ Teotihuacan/GameData/Level4Spawns.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using static Teotihuacan.Entities.Enemy.DataCategory;
+
+
 namespace Teotihuacan.GameData
 {
     public class Level4Spawns : LevelSpawnsBase
@@ -12,7 +15,139 @@ namespace Teotihuacan.GameData
         {
             var wave = AddWave();
 
-            // etc etc
+            // reminder of what you have:
+
+            wave.AddMiniWave
+            (
+                Shooter, Shooter, Shooter
+            );
+
+            wave.AddMiniWave
+            (
+                Suicider, Suicider, Suicider,
+                SuiciderFast, SuiciderFast, SuiciderFast
+            )
+            .CanSpawnAtMultipleSpots = true;
+
+            wave.AddMiniWave
+            (
+                Shooter, Shooter, Shooter,
+                Shooter, Shooter
+            )
+            .CanSpawnAtMultipleSpots = true;
+
+
+            wave.AddMiniWave
+            (
+                Shooter, Shooter, Shooter,
+                Shooter, Shooter
+            )
+            .CanSpawnAtMultipleSpots = true;
+
+            ///////////////////////////////////////
+
+            wave = AddWave();
+
+            // Introduce the new shooter:
+            wave.AddMiniWave
+            (
+                ShooterSpread
+            );
+
+            wave.AddMiniWave
+            (
+                ShooterSpread, ShooterSpread
+            );
+
+            wave.AddMiniWave
+            (
+                Shooter, Shooter, Shooter,
+                ShooterSpread, ShooterSpread
+            );
+
+            ///////////////////////////////////////
+
+            wave = AddWave();
+
+            wave.AddMiniWave
+            (
+                Suicider, SuiciderFast, Suicider, SuiciderFast
+            )
+            .CanSpawnAtMultipleSpots = true;
+
+            wave.AddMiniWave
+            (
+                ShooterSpread, ShooterSpread, SuiciderFast, SuiciderFast
+            );
+
+            wave.AddMiniWave
+            (
+                Shooter, Shooter, Shooter, 
+                Shooter, ShooterSpread, ShooterSpread
+            );
+
+            wave.AddMiniWave
+            (
+                Shooter, Shooter, 
+                Suicider, Shooter,
+                Shooter, Suicider, 
+                Shooter, Shooter,
+                Suicider
+            );
+
+
+            ///////////////////////////////////////
+
+            wave = AddWave();
+
+            wave.AddMiniWave
+            (
+                SuiciderFast, SuiciderFast, SuiciderFast,
+                SuiciderFast, SuiciderFast, SuiciderFast
+            )
+            .CanSpawnAtMultipleSpots = true;
+
+            wave.AddMiniWave
+            (
+                SuiciderFast, SuiciderFast, SuiciderFast,
+                SuiciderFast, SuiciderFast, SuiciderFast,
+                SuiciderFast, SuiciderFast, SuiciderFast
+            )
+            .CanSpawnAtMultipleSpots = true;
+
+
+            wave.AddMiniWave
+            (
+                Shooter, Shooter, Shooter,
+                Shooter, Shooter, Shooter
+            )
+            .CanSpawnAtMultipleSpots = true;
+
+            ///////////////////////////////////////
+
+            wave.AddMiniWave
+            (
+                ShooterSpread, ShooterSpread, ShooterSpread
+            )
+            .CanSpawnAtMultipleSpots = true;
+
+            wave.AddMiniWave
+            (
+                ShooterSpread, ShooterSpread,
+                Suicider, Suicider, Suicider, Suicider
+            )
+            .CanSpawnAtMultipleSpots = true;
+
+            wave.AddMiniWave
+            (
+                ShooterSpread, ShooterSpread, ShooterSpread,
+                Boss
+            )
+            .CanSpawnAtMultipleSpots = true;
+
+
+            wave = AddWave();
+
         }
     }
 }
