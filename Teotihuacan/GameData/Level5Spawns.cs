194892c790cf181f9dfb5f01304435603f5103c6
Victor Chelaru <VicChelaru@gmail.com>
2019-07-31 22:13:25 -0600

    Started adding level5 spawns

---
+++ Teotihuacan/GameData/Level5Spawns.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static Teotihuacan.Entities.Enemy.DataCategory;
+
+
+namespace Teotihuacan.GameData
+{
+    public class Level5Spawns : LevelSpawnsBase
+    {
+        public Level5Spawns()
+        {
+            var wave = AddWave();
+
+            wave.AddMiniWave
+            (
+                Shooter, Shooter, Shooter,
+                Shooter
+            );
+
+            wave.AddMiniWave
+            (
+                Shooter, Shooter, Shooter,
+                Shooter, Suicider, Suicider
+            );
+
+            wave.AddMiniWave
+            (
+                Shooter, Shooter, Shooter,
+                Shooter, Suicider, Suicider
+            );
+
+            wave.AddMiniWave
+            (
+                Shooter, Shooter, Shooter,
+                Shooter, Shooter, Shooter,
+                Shooter, Suicider, Suicider
+            )
+            .CanSpawnAtMultipleSpots = true ;
+
+
+            ////////////////////////////////////////
+
+            wave = AddWave();
+
+            wave.AddMiniWave
+            (
+                ShooterSpread, ShooterSpread
+            );
+
+            wave.AddMiniWave
+            (
+                ShooterSpread, ShooterSpread, ShooterSpread,
+                Suicider, Suicider, Suicider
+            );
+
+            wave.AddMiniWave
+            (
+                SuiciderFast, SuiciderFast, SuiciderFast,
+                SuiciderFast, SuiciderFast, SuiciderFast
+            );
+
+
+        }
+    }
+}
